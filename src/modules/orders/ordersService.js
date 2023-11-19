b6197eb533d23d390ed494b41f005b7f65fa1347
Wilson Callisaya <demarktec@gmail.com>
2023-11-19 17:33:39 -0500

    considerando la disminución del stock de los productos en la creación de la orden

--- src/modules/orders/ordersService.js
+++ src/modules/orders/ordersService.js
@@ -17,26 +17,54 @@ export class OrderService extends OrderInterface {
 
   async create(body) {
     const orderCode = await getNextCode('orderCode');
-    const itemsWithPrices = await Promise.all(body.items.map(async (item) => {
+    const productUpdates = [];
+  
+    // Verifica el stock y actualiza la lista de productos
+    for (const item of body.items) {
       const product = await this.productModel.findOne({ code: item.code });
+  
+      if (!product || product.stock < item.cantidad) {
+        // No hay suficiente stock para el producto
+        throw new Error(`No hay suficiente stock para el producto con código ${item.code}`);
+      }
+  
+      // Actualiza el stock del producto
+      product.stock -= item.cantidad;
+      productUpdates.push(product.save());
+    }
+  
+    // Espera a que todas las actualizaciones de stock se completen
+    await Promise.all(productUpdates);
+  
+    // Obtén los precios de los productos
+    const products = await this.productModel.find({ code: { $in: body.items.map(item => item.code) } });
+  
+    // Mapea los precios de los productos al cuerpo de la orden
+    const itemsWithPrices = body.items.map(item => {
+      const product = products.find(product => product.code === item.code);
       return {
         ...item,
-        precio: product.precio,
+        precio: product ? product.precio : 0, // Asumiendo un valor predeterminado si no se encuentra el producto
       };
-    }));
-
+    });
+  
     const total = itemsWithPrices.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
     const fecha = Date.now();
+  
     const orderData = {
       ...body,
       code: orderCode,
       items: itemsWithPrices,
       total,
-      fecha
+      fecha,
     };
-
-    return await this.orderModel.create(orderData);
+  
+    // Crea la orden en la base de datos
+    const order = await this.orderModel.create(orderData);
+  
+    return order;
   }
+  
 
   async update(id, body) {
     const order = await this.getById(id);
